b099753c2a8c6b277a2d3523f86b98eda201738e
Zhijun Gao <galty687@gmail.com>
2020-05-15 16:31:57 +0800

    add output

---
+++ out/webhelp-responsive/oxygen-webhelp/search/index-1.js
@@ -0,0 +1,56 @@
+// Auto generated list of analyzer stop words that must be ignored by search.
+stopWords = new Array();
+stopWords[0]= "but";
+stopWords[1]= "be";
+stopWords[2]= "with";
+stopWords[3]= "such";
+stopWords[4]= "then";
+stopWords[5]= "for";
+stopWords[6]= "no";
+stopWords[7]= "will";
+stopWords[8]= "not";
+stopWords[9]= "are";
+stopWords[10]= "and";
+stopWords[11]= "their";
+stopWords[12]= "if";
+stopWords[13]= "this";
+stopWords[14]= "on";
+stopWords[15]= "into";
+stopWords[16]= "a";
+stopWords[17]= "or";
+stopWords[18]= "there";
+stopWords[19]= "in";
+stopWords[20]= "that";
+stopWords[21]= "they";
+stopWords[22]= "was";
+stopWords[23]= "is";
+stopWords[24]= "it";
+stopWords[25]= "an";
+stopWords[26]= "the";
+stopWords[27]= "as";
+stopWords[28]= "at";
+stopWords[29]= "these";
+stopWords[30]= "by";
+stopWords[31]= "to";
+stopWords[32]= "of";
+
+var indexerLanguage="en";
+
+// Auto generated index for searching.
+w["search"]="0*0*1,1*0*1,2*0*1,3*0*1,4*0*1";
+w["什么是虚拟背景"]="4*30*0$2";
+w["什么是降噪"]="3*30*0$2";
+w["介绍如何创建腾讯会议的账号"]="1*10*-1$3";
+w["以后熟练了"]="2*0*j";
+w["关于教师丹田发声八个步骤第一步就是放松发声相关的肌肉"]="2*10*-1$3";
+w["具体方法是"]="2*0*c";
+w["再次确认离开"]="0*10*-1$4";
+w["发出声音"]="2*0*g";
+w["受到腾讯的确认推送即为注册成功"]="1*0*a";
+w["可以提升语言质量"]="3*10*-1$5";
+w["可以矫正单纯用上胸部呼吸的不良习惯和呼吸肌紧张"]="2*0*b";
+w["可以设置虚拟背景来替换背景"]="4*10*-1$8";
+w["叹气准备"]="2*0*e";
+w["叹气放松喉肌法"]="2*0*6";
+w["叹气是深呼吸的一种形式"]="2*0*a";
+
